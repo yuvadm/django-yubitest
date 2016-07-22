f3bed477ff2de5c6a4940ace6c5a0fe81fdf85e2
Yuval Adam <_@yuv.al>
2016-07-22 14:45:40 +0300

    Use OTPAdminSite

--- yubitest/urls.py
+++ yubitest/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from django_otp.admin import OTPAdminSite
+
+admin = OTPAdminSite(OTPAdminSite.name)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.urls),
 ]
